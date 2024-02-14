8c99bf67296125eed70822deb577fcedbc7b3e73
HAUCUONG <129666253+HAUCUONG@users.noreply.github.com>
2024-02-14 23:26:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = "My Love!";
+const giftUrl = "https://www.facebook.com/tram.my.7127";
+const eventName = "Happy Valentine,";
+const titleCard = "Dear My Half Orange";
+const contentCard = `Chúc em yêu của anh ngày Valentine tràn ngập niềm vui và những nụ cười nhaa. Mong những điều tốt đẹp nhất sẽ đến với em yêu trong hôm nay và cả những ngày sau nữaaa.<br/>Anh yêuuu em!🧡🧡`;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "em-yeu.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
